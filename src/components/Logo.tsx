9e44e3c2856d9acf1f151db9d15d371f5db2b245
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-12-30 13:28:25 +0200

    Header logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,11 +2,11 @@ export default function Logo() {
   return (
     <div className="flex items-center">
       <img 
-        src="/961-dot.png" 
+        src="/961-logo.png" 
         alt=""
-        className="w-2 h-2 mr-1"
+        className="mr-1 h-8"
       />
-      <span className="text-[22px] font-bold tracking-wide">961</span>
+      {/* <span className="text-[22px] font-bold tracking-wide">961</span> */}
     </div>
   );
 }
