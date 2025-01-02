0becc390f355388ca60b3f5eff863510ac804b00
Ezra Gonyuie <ezragonyuie@gmail.com>
2025-01-02 15:37:38 +0200

    Logo should be the 961 white logo instead with News written next to it. Clicking on the logo goes to 961.co and click on the news goes to news.961.co

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
     <div className="flex items-center">
-      <img 
-        src="/961-logo.png" 
-        alt=""
-        className="mr-1 h-8"
-      />
-      {/* <span className="text-[22px] font-bold tracking-wide">961</span> */}
+      <Link href="https://961.co" target="_blank">
+        <img
+          src="/961-Logo-White.png"
+          alt=""
+          className="mr-1 h-8"
+        />
+      </Link>
+      <Link href="/"><span className="text-[2rem] font-bold tracking-wide">News</span></Link>
     </div>
   );
 }
